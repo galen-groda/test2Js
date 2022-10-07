36f074c6503ebad361f9a833d09242ee88b78925
galen-groda <maksymhrystan@gmail.com>
2022-10-07 07:43:26 +0300

    Add calc for 2 prod without subtotal

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /// counter1 for alpine product
 
 let counter1 = 1;
+
 let btns1 = document.querySelectorAll('.btn1');
 
 let alpProd = document.querySelector('.alpine');
+let alpCheck = document.querySelector('.alpine_finalCheck_product');
+
+let alpPrice = 3.15
+alpCheck.textContent = alpPrice;
+
+
 
 btns1.forEach(function (btn1) {
     btn1.addEventListener('click', function (e) {
@@ -11,17 +18,22 @@ btns1.forEach(function (btn1) {
         const style1 = e.currentTarget.classList;
 
         if (style1.contains('decrease')) {
-            counter1 --
+            if (counter1 < 1) {
+                counter1 = 1
+            } else {
+                counter1 --
+            }
+
         } else if (style1.contains('increase')) {
             counter1 ++
         }
-        alpProd.textContent = counter1
-
+        alpProd.textContent = counter1;
 
         let alpCheck = document.querySelector('.alpine_finalCheck_product');
 
 
         let alpAmount = counter1
+
         let alpPrice = alpAmount*3.15;
 
         alpCheck.textContent = alpPrice  + ' $'
@@ -31,18 +43,31 @@ btns1.forEach(function (btn1) {
 
 /// counter2 for milka product
 
-let counter2 = 1
+let counter2 = 1;
+
 let btns2 = document.querySelectorAll('.btn2');
 
 let milkProd = document.querySelector('.milka');
+let milkCheck = document.querySelector('.milka_finalCheck_product');
+
+let milkPrice = 4.99;
+milkCheck.textContent = milkPrice;
+
+
 
 btns2.forEach(function (btn2) {
     btn2.addEventListener('click', function (e) {
+        console.log(e.currentTarget.classList);
         const style2 = e.currentTarget.classList;
 
-        if (style2.contains('decrease') ) {
-            counter2 --
-        } else if (style2.contains ('increase')) {
+        if (style2.contains('decrease')) {
+            if (counter2 < 1) {
+                counter2 = 1
+            } else {
+                counter2 --
+            }
+
+        } else if (style2.contains('increase')) {
             counter2 ++
         }
         milkProd.textContent = counter2;
@@ -50,14 +75,21 @@ btns2.forEach(function (btn2) {
         let milkCheck = document.querySelector('.milka_finalCheck_product');
 
 
-        let milkAmount = counter2
+        let milkAmount = counter2;
+
         let milkPrice = milkAmount*4.99;
 
         milkCheck.textContent = milkPrice  + ' $'
-
     })
 })
 
 
+let finalCheck = document.querySelector('.test1');
+let btnCheckout = document.querySelector(".btn_check");
 
+btnCheckout.addEventListener('click', function () {
+
+    let finTot = milkProd + alpProd;
+    finalCheck.textContent = finTot + " $";
+})
 
